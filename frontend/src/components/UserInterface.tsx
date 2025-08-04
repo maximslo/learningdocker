37555c3eecf0187d15a1b376f0913915e623caf9
Maxim Slobodchikov <maxims@bu.edu>
2025-08-03 23:56:41 -0400

    form added

--- frontend/src/components/UserInterface.tsx
+++ frontend/src/components/UserInterface.tsx
@@ -15,7 +15,6 @@ interface User {
 
 const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000'
 
-
 export default function UserInterface({ backendName }: UserInterfaceProps) {
     const [users, setUsers] = useState<User[]>([])
     const [newUser, setNewUser] = useState({ name: '', email: '' })
@@ -26,6 +25,7 @@ export default function UserInterface({ backendName }: UserInterfaceProps) {
         const fetchData = async () => {
             try {
                 const response = await axios.get(`${apiUrl}/api/${backendName}/users`);
+                console.log(response)
                 setUsers(response.data.reverse())
             } catch (error) {
                 console.error('Error fetching data:', error)
@@ -35,11 +35,23 @@ export default function UserInterface({ backendName }: UserInterfaceProps) {
         fetchData();
     }, [backendName, apiUrl])
 
+    // Create user
+    // const createUser = 
+
     return <div>
         <h1>{backendName}</h1>
+
+        <form>
+            <input placeholder='Name:'></input>
+            <input placeholder='Email:'></input>
+        </form>
+
         <div>
             {users.map((user) => (
-                <CardComponent card={user}/>
+                <div key={user.id}>
+                    <CardComponent card={user}/>
+                    {/* <button onClick={() => deleteUser(user.id)}>Delete User</button> */}
+                </div>
             ))}
         </div>
         {/* <CardComponent card={{ id: 1, name: "John Doe", email: "email" }} /> */}
